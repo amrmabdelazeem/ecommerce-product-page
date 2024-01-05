ef7f571476a61368c68885c2405d4f32abd7476a
Amr Elsehety <amr.abdelazeem22@gmail.com>
2024-01-05 14:50:50 +0300

    adjust styling for button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ export default function Button({ title, onCart }) {
   return (
     <button
       onClick={onCart}
-      className="bg-primary w-full h-12 rounded-xl font-700 text-white text-sm flex items-center justify-center gap-3"
+      className="bg-primary w-full h-12 rounded-xl font-700 text-white text-sm flex items-center justify-center gap-3 xl:h-14 xl:text-base"
     >
       {title}
     </button>
