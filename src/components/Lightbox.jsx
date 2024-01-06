20c42f73de55d46273df7992c894b14a38b03e55
Amr Elsehety <amr.abdelazeem22@gmail.com>
2024-01-06 14:10:04 +0300

    Lightbox can change images based on choice

--- src/components/Lightbox.jsx
+++ src/components/Lightbox.jsx
@@ -1,8 +1,12 @@
 /* eslint-disable react/prop-types */
-
+import { useState } from "react";
 
 export default function Lightbox({toggle, onCloseBox}) {
+  const[currentProduct, setCurrentProduct] = useState(1);
 
+  function handleProductImage(id){
+    setCurrentProduct(id);
+  }
   const thumbnails = [
     "src/assets/images/image-product-1-thumbnail.jpg",
     "src/assets/images/image-product-2-thumbnail.jpg",
@@ -11,6 +15,7 @@ export default function Lightbox({toggle, onCloseBox}) {
   ];
   return (
     <section id="lightbox" className={`xl:absolute xl:grid xl:h-full w-3/4 xl:place-content-center xl:place-items-center z-30 ${toggle ? 'xl:visible' : 'xl:hidden'}`}>
+    <div className={`absolute w-screen h-full bg-black opacity-55 top-0 left-0 -z-10 ${toggle ? "visible" : "hidden" }`}></div>
       <button className="xl:place-self-end xl:size-6 xl:mb-3">
         <img className="xl:size-6 "
           src="src/assets/images/icon-close.svg"
@@ -18,11 +23,11 @@ export default function Lightbox({toggle, onCloseBox}) {
           onClick={onCloseBox}
         />
       </button>
-      <img className="relative xl:w-550 xl:h-550 xl:grid xl:place-items-center xl:mb-4 xl:rounded-xl" src="src/assets/images/image-product-1.jpg" alt="" />
+      <img className="relative xl:w-550 xl:h-550 xl:grid xl:place-items-center xl:mb-4 xl:rounded-xl" src={`src/assets/images/image-product-${currentProduct}.jpg`} alt="" />
       <div className="">
         {thumbnails.map((thumbnail, index)=>{
             return(
-            <button  key={index}>
+            <button  key={index} id={index} onClick={()=>handleProductImage(index+1)}>
                 <img className="size-24 rounded-lg cursor-pointer xl:mx-4" src={thumbnail} alt="image thumbnail" />
             </button>
             )
