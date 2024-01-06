43cf4d84fdfaa73ef40a992c325d59057adc5866
Amr Elsehety <amr.abdelazeem22@gmail.com>
2024-01-06 17:38:16 +0300

    Add thumbnail array

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,10 +1,16 @@
 /* eslint-disable react/prop-types */
 import { useState } from "react";
 
-export default function Carousel({onBox}) {
+export default function Carousel({ onBox }) {
   const [index, setIndex] = useState(1);
   const [current, SetCurrent] = useState(false);
-  
+
+  const thumbnails = [
+    "src/assets/images/image-product-1-thumbnail.jpg",
+    "src/assets/images/image-product-2-thumbnail.jpg",
+    "src/assets/images/image-product-3-thumbnail.jpg",
+    "src/assets/images/image-product-4-thumbnail.jpg",
+  ];
 
   let lastItem = 4;
   function handleNext() {
@@ -20,6 +26,11 @@ export default function Carousel({onBox}) {
   }
   function handleClick() {
     SetCurrent(!current);
+    
+  }
+
+  function handleProductToShow(id){
+    setIndex(id +1);
   }
 
   return (
@@ -30,27 +41,35 @@ export default function Carousel({onBox}) {
       <img
         src={`src/assets/images/image-product-${index}.jpg`}
         alt={`Sneakrs product`}
-        className="w-full h-full object-cover xl:object-cover xl:rounded-2xl xl:w-9/12"
+        className="w-full h-full object-cover xl:object-cover xl:rounded-2xl xl:w-8/12"
         onClick={onBox}
       />
       <div
         id="gallery"
         className="hidden xl:visible xl:mt-5 xl:grid xl:grid-cols-4 xl:gap-8"
       >
-          <button
-            onClick={handleClick}
-            className={`${
-              current ? "border-primary border-4 rounded-xl" : "border-none"
-            }`}
-          >
-            <img
-              className={`size-20 rounded-lg cursor-pointer hover:opacity-65 ${
-                current ? "opacity-65" : "border-none"
+        {thumbnails.map((thumbnail, id) => {
+          return (
+            <button
+              key={id}
+              id={id}
+              onClick={()=>{
+                handleProductToShow(id);
+              }}
+              className={`${
+                current ? "border-primary border-4 rounded-xl" : "border-none"
               }`}
-              src={`src/assets/images/image-product-1-thumbnail.jpg`}
-              alt=""
-            />
-          </button>
+            >
+              <img
+                className={`size-20 rounded-lg cursor-pointer hover:opacity-65 ${
+                  current ? "opacity-65" : "border-none"
+                }`}
+                src={thumbnail}
+                alt=""
+              />
+            </button>
+          );
+        })}
       </div>
       <button
         onClick={handleNext}
