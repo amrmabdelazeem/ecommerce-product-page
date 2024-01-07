ad1bc0d8105faa97972199289f395b33f5c60fbb
Amr Elsehety <amr.abdelazeem22@gmail.com>
2024-01-07 18:41:30 +0300

    Change side length height

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -8,14 +8,15 @@ export default function Nav({ cartNotify, count, onDelete}) {
 
   function handleSideMenu() {
     setIsClicked(!isClicked);
+    setIsCart(false);
   }
 
   function handleCart() {
     setIsCart(!isCart);
   }
   return (
-    <nav className={`bg-white border-gray-200 flex justify-between xl:border-b-2 xl:border-gray-200 xl:pt-10 xl:pb-9 col-span-3 xl:px-32`}>
-      <div className={`absolute w-screen h-160 bg-black opacity-55 top-0 xl:left-0 z-10 ${isClicked ? "visible" : "hidden"}`}></div>
+    <nav className={`bg-white border-gray-200 flex justify-between xl:border-b-2 xl:border-gray-200 xl:pt-10 xl:pb-9 col-span-3 xl:mx-36 xl:mb-20`}>
+      <div className={`absolute w-screen h-150 bg-black opacity-55 top-0 xl:left-0 z-10 xl:h-130 ${isClicked ? "visible" : "hidden"}`}></div>
 
       <Cart
         toggleCart={isCart}
@@ -39,7 +40,7 @@ export default function Nav({ cartNotify, count, onDelete}) {
           />
         </a>
         <div
-        className={`absolute w-60 h-160 bg-lightBlue left-0 top-0 p-6 pt-7 ${
+        className={`absolute w-60 h-150 bg-lightBlue left-0 top-0 p-6 pt-7 ${
           isClicked ? "visible" : "hidden"
         } z-20 xl:static xl:bg-transparent xl:w-full xl:h-auto xl:p-0 xl:pt-0 xl:flex`}
       >
@@ -69,9 +70,9 @@ export default function Nav({ cartNotify, count, onDelete}) {
         </ul>
       </div>
       </div>
-      <div className="flex items-center gap-4 p-6  xl:gap-8  xl:p-0">
+      <div className="flex items-center gap-4 p-6  xl:gap-10  xl:p-0">
         <button className="relative " onClick={handleCart}>
-        <svg className="size-5 xl:size-6 xl:fill-gray-400 xl:hover:fill-black" viewBox="0 0 25 25"><path d="M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z"/></svg>
+        <svg className="size-5 xl:size-8 xl:fill-gray-400 xl:hover:fill-black" viewBox="0 0 25 25"><path d="M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z"/></svg>
           <span
             className={`absolute bg-primary w-6 h-4 rounded-xl -top-1 text-xs text-white ${
               cartNotify ? "visible" : "hidden"
@@ -81,7 +82,7 @@ export default function Nav({ cartNotify, count, onDelete}) {
           </span>
         </button>
         <img
-          className="size-7 xl:size-12 xl:hover:border-primary xl:hover:border-2 xl:rounded-full cursor-pointer"
+          className="size-7 xl:size-16 xl:hover:border-primary xl:hover:border-2 xl:rounded-full cursor-pointer"
           src="src/assets/images/image-avatar.png"
           alt="Avatar of the current user"
         />
